d10eead2a4c2f427c83588934f82ea25b055ce46
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-05-15 21:41:01 +0600

    jwt token functionality added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const app = express()
 const cors = require('cors')
@@ -11,11 +13,22 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
+        'https://book-nest-d0b87.web.app',
+        'book-nest-d0b87.firebaseapp.com'
     ],
-    credential: true
+    credentials: true
 }))
 app.use(express.json())
+app.use(cookieParser())
 
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
 
 // mongodb connections
 
@@ -30,6 +43,30 @@ const client = new MongoClient(uri, {
     }
 });
 
+//----------------------------
+const logger = (req, res, next) => {
+    console.log("Log info", req.method, req.url)
+    next()
+}
+// verify token
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    // console.log('token in the middleware', token)
+    if (!token) {
+        return res.status(401).send('unauthorized access')
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send('unauthorized access')
+        }
+        req.user = decoded
+        next()
+    })
+}
+
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -38,10 +75,31 @@ async function run() {
         // collections
         const bookCollection = client.db("BookNest").collection('bookCollection')
         const categoriesCollection = client.db("BookNest").collection('categories')
-        const borrowedCollection =client.db("BookNest").collection('borrowedBooks')
+        const borrowedCollection = client.db("BookNest").collection('borrowedBooks')
+
+
+        // token generating here
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user)
+            const token = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            }).send({ success: true })
+
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user)
+            res.clearCookie('token', { maxAge: 0 }).send({ success: 'coockie cleared' })
+        })
 
         // server and client collaboration starts here
         app.get('/books', async (req, res) => {
+
             const cursor = bookCollection.find()
             const result = await cursor.toArray()
             res.send(result)
@@ -118,11 +176,15 @@ async function run() {
             res.send({ Success: `updated ${bookId}` })
         })
 
-        app.delete('/borrowedBooks/:email', async (req, res) => {
+        app.delete('/borrowedBooks/:email', logger, verifyToken, async (req, res) => {
             const email = req.params.email;
+            console.log('token owner', req.user)
+            if (req.user.email !== req.query.email) {
+                res.status(403).send({ message: 'forbidden access' })
+            }
             // console.log(bookId)
             const query = { borrowerEmail: email };
-            
+
             const result = await borrowedCollection.deleteOne(query)
             res.send({ Success: `deleted from ${email}` })
         })
@@ -138,23 +200,23 @@ async function run() {
             const data = req.body;
             console.log(data);
             const result = await borrowedCollection.insertOne(data)
-            res.send({success: 'book added to borrowed list'})
+            res.send({ success: 'book added to borrowed list' })
         })
 
-        app.get('/borrowedBooks', async(req, res)=>{
+        app.get('/borrowedBooks', async (req, res) => {
             const cursor = borrowedCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.get('/borrowedBooks/:email', async(req, res)=>{
+        app.get('/borrowedBooks/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {borrowerEmail: email}
+            const query = { borrowerEmail: email }
             const result = await borrowedCollection.find(query).toArray()
             res.send(result)
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
